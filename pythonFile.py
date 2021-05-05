35e447f1bc00615e88ad75dc8bf1a69976f42602
blackdoor73 <yashag2910@gmail.com>
2021-05-05 16:21:55 +0530

    localhost implementation

---
+++ pythonFile.py
@@ -0,0 +1,36 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+from urllib.parse import urlparse
+# query_components = { "imsi" : "Hello" }
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        a = query_components["a"]
+        b = query_components["b"]
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server. <h1>"+str(a+b)+"</h1></p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+        
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
